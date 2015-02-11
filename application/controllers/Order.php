423362e6788e68df07a819b9dacd995bb4f4e8e8
Paolo Montano <Paolo.Montano@Gmail.com>
2015-02-11 12:09:25 -0800

    Checkout and validation.

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -87,7 +87,15 @@ class Order extends Application {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        $this->data['total'] = money_format('%n', $this->orders->total($order_num));
+        $items = $this->orderitems->group($order_num);
+
+        foreach ($items as $item) {
+          $menuitem = $this->menu->get($item->item);
+          $item->code = $menuitem->name;
+        }
+
+        $this->data['items'] = $items;
 
         $this->render();
     }
